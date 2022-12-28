93fcd1fe7ecd3c6d7febf6730b08eb56886b90f5
Seeun <tsodinq@gmail.com>
2022-12-27 22:51:08 -0600

    small changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const envutils = __importStar(require("./env-utils"));
-module.exports = envutils;
+exports.setEnvVar = exports.appendEnvVar = void 0;
+const fs_1 = require("fs");
+function appendEnvVar(path, key, value) {
+    (0, fs_1.appendFileSync)(path, `${key}=${value}`, { encoding: "utf8" });
+}
+exports.appendEnvVar = appendEnvVar;
+function setEnvVar(path, key, value) {
+    const env = (0, fs_1.readFileSync)(path, { encoding: "utf8" });
+    const lines = env.split("\n");
+    const line = lines.find((line) => line.startsWith(`${key}=`));
+    if (line) {
+        const index = lines.indexOf(line);
+        lines[index] = `${key}=${value}`;
+    }
+    else {
+        lines.push(`${key}=${value}`);
+    }
+    (0, fs_1.writeFileSync)(path, lines.join("\n"), { encoding: "utf8" });
+}
+exports.setEnvVar = setEnvVar;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import * as envutils from "./env-utils";
+import { appendFileSync, readFileSync, writeFileSync } from "fs";
 
-module.exports = envutils;
+function appendEnvVar(path: string, key: string, value: string) {
+  appendFileSync(path, `${key}=${value}`, { encoding: "utf8" });
+}
+
+function setEnvVar(path: string, key: string, value: string) {
+  const env = readFileSync(path, { encoding: "utf8" });
+
+  const lines = env.split("\n");
+  const line = lines.find((line) => line.startsWith(`${key}=`));
+
+  if (line) {
+    const index = lines.indexOf(line);
+    lines[index] = `${key}=${value}`;
+  } else {
+    lines.push(`${key}=${value}`);
+  }
+
+  writeFileSync(path, lines.join("\n"), { encoding: "utf8" });
+}
+
+export { appendEnvVar, setEnvVar };
